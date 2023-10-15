641cc8714ada0fb944279e6d39ad7b1d3c063458
Sergey Pavlov <s.a.pavlov@mail.ru>
2023-10-15 21:56:10 +0300

    test

---
+++ src/main/resources/static/assets/Keys-c3dbec4b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,a as s}from"./index-bcaf7c33.js";const t={},n=s("h4",null,"Страница находится в разработке...",-1),_=[n];function a(r,l){return c(),o("div",null,_)}const f=e(t,[["render",a]]);export{f as default};
