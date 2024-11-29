43cc22e503c93f971ceb8f3c153c30e464d75eb8
Sergey Pavlov <s.a.pavlov@mail.ru>
2024-11-29 11:42:59 +0300

    keytypes in btskeyrel

--- src/main/java/com/example/red/Controller/KeyTypeController.java
+++ src/main/java/com/example/red/Controller/KeyTypeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.red.Model.KeyType;
@@ -26,7 +27,7 @@ private final KeyTypeService keyTypeService;
 private final AboutBtsKeysRelService aboutBtsKeysRelService;
 	
 	@GetMapping("/keytypes")
-	public ResponseEntity<List<KeyType>> getKeyTypesByAboutBtsId(@PathVariable int btsId){
+	public ResponseEntity<List<KeyType>> getKeyTypesByAboutBtsId(@RequestParam(required=false) int btsId){
 	
 		try { List<Integer> keysId = aboutBtsKeysRelService.getKeyIdByAboutBtsId(btsId);
 		
